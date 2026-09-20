c819ad721eb4ad35d9cc9aa6d374582b73ebb60e
Ananya Virwani <ananyavirwani255@gmail.com>
2026-09-20 19:03:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
-let url=location.url
+let url=location.href
+
 
 let len=url.length
 
-alert("The length of the URL is: ",len)
+alert("The length of the URL is: "+len)
